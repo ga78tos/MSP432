e9a1c2c25362442059b229a6e0a5d8e7cbc0e80a
ga78tos <manuel.dellantonio.95@gmail.com>
2017-09-11 11:45:15 +0200

    Add files via upload

---
+++ carrier/Debug/subdir_vars.mk
@@ -0,0 +1,42 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432p401r.cmd 
+
+LIB_SRCS += \
+../msp432p4xx_driverlib.lib 
+
+C_SRCS += \
+../initGpio.c \
+../main.c \
+../msp432_startup_ccs.c 
+
+OBJS += \
+./initGpio.obj \
+./main.obj \
+./msp432_startup_ccs.obj 
+
+C_DEPS += \
+./initGpio.pp \
+./main.pp \
+./msp432_startup_ccs.pp 
+
+C_DEPS__QUOTED += \
+"initGpio.pp" \
+"main.pp" \
+"msp432_startup_ccs.pp" 
+
+OBJS__QUOTED += \
+"initGpio.obj" \
+"main.obj" \
+"msp432_startup_ccs.obj" 
+
+C_SRCS__QUOTED += \
+"../initGpio.c" \
+"../main.c" \
+"../msp432_startup_ccs.c" 
+
+
